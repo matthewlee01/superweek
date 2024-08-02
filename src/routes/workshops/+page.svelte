136969e82811a7ffac2ecbf94c5c759f7674b4a0
matthew lee <laughew@gmail.com>
2024-08-02 15:30:28 -0700

    workshop topics

--- src/routes/workshops/+page.svelte
+++ src/routes/workshops/+page.svelte
@@ -6,6 +6,16 @@
     decisions affect our relationship with God. our “education” day will include
     a variety of workshops that aim to integrate our faith with everyday life.
   </p>
+  <h3>topic list:</h3>
+  <p>
+    - what is the difference between accountability, discipleship, and
+    mentorship?
+  </p>
 
-	<p>etc etc</p>
+  <p>- how do i avoid addictions and work on my mental health?</p>
+  <p>- how can i prepare for post-secondary life (for HS)?</p>
+
+  <p>- how can i discern God’s voice?</p>
+
+  <p>- how can i marry the right person? p</p>
 </div>
